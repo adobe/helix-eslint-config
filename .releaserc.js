b8d8570ae6043b9d229a11a67e1cfd5277004421
Lars Trieloff <trieloff@adobe.com>
2019-08-29 10:19:18 +0000

    fix(release): disable git plugin to prevent tagging
    tagging seems to fail, because the 1.0.0 tag isn't detected.

--- .releaserc.js
+++ .releaserc.js
@@ -6,10 +6,10 @@ module.exports = {
       "changelogFile": "CHANGELOG.md",
     }],
     "@semantic-release/npm",
-    ["@semantic-release/git", {
+/*    ["@semantic-release/git", {
       "assets": ["package.json", "package-lock.json", "CHANGELOG.md"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }],
+    }], */
     ["@semantic-release/github", {}]
   ]
 };
